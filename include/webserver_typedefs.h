bd72e50cbade1b87c2a5299d747f0b87301d3522
Andrew Herren <andrew.herren@gmail.com>
2025-08-27 06:24:20 -0400

    typedefs fix

--- include/webserver_typedefs.h
+++ include/webserver_typedefs.h
@@ -3,10 +3,18 @@
 
 #if defined(ESP32)
 #include <WebServer.h>
-typedef WebServer WebServerClass;
+// Define WebServerClass to match the forward declaration in web_request.h
+class WebServerClass : public WebServer {
+public:
+  WebServerClass(uint16_t port = 80) : WebServer(port) {}
+};
 #elif defined(ESP8266)
 #include <ESP8266WebServer.h>
-typedef ESP8266WebServer WebServerClass;
+// Define WebServerClass to match the forward declaration in web_request.h
+class WebServerClass : public ESP8266WebServer {
+public:
+  WebServerClass(uint16_t port = 80) : ESP8266WebServer(port) {}
+};
 #endif
 
 #endif // WEB_MODULE_INTERFACE_WEBSERVER_TYPEDEFS_H
