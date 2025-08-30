91fcd9ffc380d658bab9d02e1fd92cee3cc3296d
Andrew Herren <andrew.herren@gmail.com>
2025-08-30 09:54:14 -0400

    revert styles location and library changes

--- include/web_module_interface.h
+++ include/web_module_interface.h
@@ -1,11 +1,11 @@
 #ifndef WEB_MODULE_INTERFACE_H
 #define WEB_MODULE_INTERFACE_H
 
-#include "../assets/web_ui_styles.h"
 #include "auth_types.h"
 #include "web_module_types.h"
 #include "web_request.h"
 #include "web_response.h"
+#include "web_ui_styles.h"
 #include "webserver_typedefs.h"
 #include <Arduino.h>
 #include <functional>
